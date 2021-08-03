dc56b8a04d9e7db887af72b81886284d0b157415
Apoorva Bidyanta <apoorva.bid12@gmail.com>
2021-08-03 16:41:26 +0530

    added and made changes in App.js file install bootstrap react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
+
+class App extends React.Component {
+  render() {
+    return (
+       <>
+
+      <html>
+      <body>
+      <p>hello</p>
+      </body>
+      </html>
+      </>
+    );
+
+
+
+    
+  }
 }
 
 export default App;
