9c2aedd54a6fefddd6cd2691893c5ae92a4a0405
Apoorva Bidyanta <apoorva.bid12@gmail.com>
2021-08-03 16:49:20 +0530

    modified App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from "react";
-
+import UserComponent from "./components/UserComponent"
 import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 
 class App extends React.Component {
   render() {
     return (
-       <>
+      <>
 
-      <html>
-      <body>
-      <p>hello</p>
-      </body>
-      </html>
+
+      <Router>
+      <Switch>
+          <Route>
+            <UserComponent />
+
+        </Route>
+
+      </Switch>
+      </Router>
       </>
     );
 
